07c462160e991f5910bea4ea9d8e56e90dca1f55
christopher <christophervalle3339@gmail.com>
2025-05-11 05:26:24 -0700

    Finish logic for book id generation, and finish Book class

--- src/library/books/Book.java
+++ src/library/books/Book.java
@@ -2,28 +2,56 @@ package library.books;
 
 public class Book {
     private String author;
-    private String bookName;
+    private String title;
     private int bookId;
+    private boolean isBorrowed;
 
+    //defines all informatino needed per book
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+        this.bookId = BookIdGenerator.generateId();
+        this.isBorrowed = false;
+    }
 
     //setters for book **below**
-    //should return int ID
-    public void getId() {}
+    public int getId() {
+        return bookId; //returns generated id 
+    }
 
-    public void getTitle() {}
+    public String getTitle() {
+        return title;
+    }
 
-    public void getAuthor() {}
-
+    public String getAuthor() {
+        return author;
+    }
     //***end of setters***
 
-    public void isAvailable() {}
+    //checks if book is currently borrowed
+    public String isAvailable() {
+        if(!isBorrowed) { //if false it means book is available
+            return "This book is available";
+        }
+        else
+            return "This book is not available";
+    }
 
-    public void borrow() {}
+    //marks the books as borrowed
+    public void borrow() {
+            isBorrowed = true; //call this method when book gets borrow by user
+    }
 
-    public void returnBook() {}
+    public void returnBook() {
+        isBorrowed = false; //call this method when book become available
+    }
 
     @Override
-    public String toString() {return "s";} //display book info
+    public String toString() {
+        return String.format("Author: %s | Book Title: %s | %s",
+        author, title, 
+        !isBorrowed ? "available" : "is not available");
+    } //display book info
 
 
 
--- src/library/books/BookIdGenerator.java
+++ src/library/books/BookIdGenerator.java
@@ -1,5 +1,20 @@
 package library.books;
 
 public class BookIdGenerator {
-    
+
+    private static int nextBookID = 1;
+
+    //generates unique id for each book
+    public static int generateId() {
+        return nextBookID++;
+    }
+
+    //allows you to see what id num comes next **just for testing**
+    public static int peekNextId() {
+        return nextBookID;
+    }
+
+
+
+
 }
